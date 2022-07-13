d835fbe4c40f76f85d665be02088fd52a9bc6b67
Alec Venida <97048214+avenida714@users.noreply.github.com>
2022-07-12 23:11:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Main from "./Main";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
+//let's try this change now
+//how about now
+//still not sure
